0d23976563caadc1316b14405a25bd6849a20f08
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-07-04 03:45:03 +0430

    Added Book Model And Updated Database

---
+++ Model/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Book> Book { get; set; }
+
+    }
+}
