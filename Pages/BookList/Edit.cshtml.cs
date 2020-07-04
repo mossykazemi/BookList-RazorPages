4a5370c7a198930892ea3b838fb0b964fc55879f
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-07-05 03:35:44 +0430

    Added Edit page model

---
+++ Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class EditModel : PageModel
+    {
+        private ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }  
+        
+
+        [BindProperty] 
+        public Book Book { get; set; }
+
+
+        public async Task OnGet(int id)
+        {
+            Book = await _db.Book.FindAsync(id);
+        }
+    }
+}
