f0a67f42e622fcca686bdcd974e1c853ff4c7f9c
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-07-05 05:22:56 +0430

    added delete function and button to sweet alert

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
 
 namespace BookListRazor.Controllers
 {
@@ -19,9 +21,23 @@ namespace BookListRazor.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
-            return Json(new {data = _db.Book.ToList()});
+            return Json(new {data = await _db.Book.ToListAsync()});
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb =await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookFromDb == null)
+            {
+                return Json(new {success = false, message = "Error while Deleting"});
+            }
+
+            _db.Book.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new {success = true, message = "Delete Successful"});
         }
     }
 }
